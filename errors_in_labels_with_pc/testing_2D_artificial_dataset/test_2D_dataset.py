95bf6b912015a5a586caeb950591c6d04fcffec3
maxnasc7 <maxnasc7@outlook.com>
2025-03-25 13:47:12 -0300

    correction for 1d class on multiclass done. Need to test on other types of datasets

--- errors_in_labels_with_pc/testing_2D_artificial_dataset/test_2D_dataset.py
+++ errors_in_labels_with_pc/testing_2D_artificial_dataset/test_2D_dataset.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 x = np.linspace(-2, 2, num=101)
 media_ruido = 0; 
-var_ruido = 0.5
+var_ruido = 0.8
 ruido = media_ruido + (var_ruido * np.random.randn(x.shape[0]))
 y = x**2 + ruido
 
@@ -36,18 +36,31 @@ distances = {i: curve.map_to_arcl(X) for i, curve in enumerate(curves_)}
 
 # Junta as informações das distâncias para obter, em cada índice de dados do treinamento as distâncias para cada ponto
 data_distances = pd.DataFrame({chave: dado[1] for chave, dado in distances.items()})
-data_distances['original_labels'] = Y
+data_distances['original_label'] = Y
 # Para cada ponto é verificada qual a classe mais próxima e comparada com a classe indicada no início, caso exista algum erro de rótulo o código altera a label do dado para melhor ajustar os dados
-# for i, row in data_distances.iterrows():
-    
-
-a=1
-
+rows_to_adjust = []
+for i, row in data_distances.iterrows():
+    min_dist = row.iloc[:-1].idxmin(); # Classe cujo ponto tem menor distância
+    min_value = row.iloc[:-1].min()
+    if row['original_label'] != min_dist:
+        rows_to_adjust.append(i)
+        
 fig, ax = plt.subplots()
 ax.plot(c1[:,0], c1[:,1], 'o')
 ax.plot(c2[:,0], c2[:,1], 'r*')
 curves_[0].plot_curve(ax)
 curves_[1].plot_curve(ax)
 
+for i in rows_to_adjust:
+    Y[i] = 1 if Y[i] == 0 else 0  # Inverte 0 ↔ 1
+
+# **Plotando os dados ajustados**
+fig, ax = plt.subplots()
+ax.scatter(X[Y.flatten() == 0][:, 0], X[Y.flatten() == 0][:, 1], color='blue', label='Classe 0 (ajustada)')
+ax.scatter(X[Y.flatten() == 1][:, 0], X[Y.flatten() == 1][:, 1], color='red', marker='*', label='Classe 1 (ajustada)')
+
+# **Desenhando as curvas ajustadas**
+curves_[0].plot_curve(ax)
+curves_[1].plot_curve(ax)
 # print(distances)
 plt.show()
